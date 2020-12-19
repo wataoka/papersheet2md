58e0e27b4dc122f19fd0e1fce851a7a9feaa8a4f
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-12-19 10:53:25 +0900

    add: NOTE_DIR, NOTE_FILEPATH_LIST, NOTE_FILENAME_LIST, and NOTE_FILEID_LIST in constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 import os
+from glob import glob
 
 # dir
 HOME_DIR : str = os.path.expanduser('~')
 BASE_DIR : str = os.path.dirname(os.path.abspath(__file__))
+NOTE_DIR : str = os.path.join(BASE_DIR, 'note')
 
 # path
 APIKEY_PATH : str = "<PATH to Your Google API KEY>"
@@ -11,3 +13,12 @@ SCOPE             : list = ['https://spreadsheets.google.com/feeds', 'https://ww
 
 SPREADSHEETS_NAME : str  = "<Your Spreadsheet File Name>"
 SHEETS_NAME       : str  = "<Your Sheet Name>"
+
+# note
+NOTE_FILEPATH_LIST : list = glob(f'{NOTE_DIR}/*.txt')
+NOTE_FILENAME_LIST : list = []
+NOTE_FILEID_LIST   : list = []
+for filepath in NOTE_FILEPATH_LIST:
+    NOTE_FILENAME_LIST.append(filepath.split('/')[-1])
+for filename in NOTE_FILENAME_LIST:
+    NOTE_FILEID_LIST.append(filename.split('.')[0])
