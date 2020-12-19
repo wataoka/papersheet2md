88fade87fd8afff5e4abedd440e985e84baa1e68
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-12-19 10:10:50 +0900

    add: scope, spreadsheets_name, and sheets_name in constants.py

--- constants.py
+++ constants.py
@@ -7,3 +7,7 @@ BASE_DIR : str = os.path.dirname(os.path.abspath(__file__))
 # path
 APIKEY_PATH : str = "<PATH to Your Google API KEY>"
 SHEETS_KEY  : str = "<KEY of Your Google Speadsheet>"
+SCOPE             : list = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+SPREADSHEETS_NAME : str  = "<Your Spreadsheet File Name>"
+SHEETS_NAME       : str  = "<Your Sheet Name>"
