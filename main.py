39f1e6c1979e2508cccc79b97085eca6ca82d58c
wataoka <wataoka@stu.kobe-u.ac.jp>
2020-12-19 10:11:38 +0900

    add: scripts for generate markdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import pandas as pd
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -15,7 +16,6 @@ from constants import (
 )
 
 
-
 def get_sheet(sheet_name):
     credentials = ServiceAccountCredentials.from_json_keyfile_name(APIKEY_PATH, SCOPE)
     client = gspread.authorize(credentials)
@@ -27,8 +27,59 @@ def get_sheet(sheet_name):
     return sheet
 
 
-def main():
+def main(filename='output.md'):
+
+    print('loading google sheet...')
     sheet = get_sheet(SHEETS_NAME)
+    sheet_df = pd.DataFrame(sheet.get_all_records())
+
+    row = 62
+    print('writing markdown...')
+    with open(filename, 'w') as f:
+        while True:
+
+            # get and check
+            row_df = sheet_df[row:row+1]
+            if len(row_df) == 0:
+                break
+
+            # set values
+            title_en = row_df['論文名'].values[0]
+            title_ja = row_df['論文名(日本語)'].values[0]
+            tag      = row_df['タグ'].values[0]
+            conf     = row_df['学会'].values[0]
+            url      = row_df['リンク'].values[0]
+            date     = row_df['投稿日付'].values[0]
+            abst     = row_df['概要'].values[0]
+            method   = row_df['手法'].values[0]
+            result   = row_df['結果'].values[0]
+            comment  = row_df['コメント'].values[0]
+
+            assert not title_en == ''
+            print(f'\n## {title_en}', file=f)
+            if not title_ja == '':
+                print(f'wataokaの日本語訳: {title_ja}', file=f)
+            if not tag == '':
+                print(f'- 種類: {tag}', file=f)
+            if not conf == '':
+                print(f'- 学会: {conf}', file=f)
+            if not date == '':
+                print(f'- 日付: {date}', file=f)
+            if not url == '':
+                print(f'- URL: [{url}]({url})', file=f)
+            if not abst == '':
+                print(f'\n### 概要', file=f)
+                print(f'{abst}', file=f)
+            if not method == '':
+                print(f'\n### 手法', file=f)
+                print(f'{method}', file=f)
+            if not result == '':
+                print(f'\n### 結果', file=f)
+                print(f'{result}', file=f)
+            if not comment == '':
+                print(f'\n### wataokaのコメント', file=f)
+                print(f'{comment}', file=f)
+            row += 1
 
 
 if __name__ == "__main__":
